0cac738f1719b9a7ea31657f65305249152b8a7f
Cao Le Tuong Nhan <caoletuongnhan072592@gmail.com>
2025-06-09 16:39:27 +0700

    Fix lại LoginForm

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { CssBaseline } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './context/AuthContext';
 import { BookProvider } from './context/BookContext';
@@ -13,7 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <BookProvider>
-          <CssBaseline />
           <App />
         </BookProvider>
       </AuthProvider>
