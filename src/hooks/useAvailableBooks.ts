34ea948fee5c193ff2ad861272913d384db75d5c
Cao Le Tuong Nhan <caoletuongnhan072592@gmail.com>
2025-08-01 21:39:00 +0700

    update code.

--- src/hooks/useAvailableBooks.ts
+++ src/hooks/useAvailableBooks.ts
@@ -4,21 +4,21 @@ import { type Book } from '../types';
 import { useBookContext } from '../context/BookContext';
 
 export const useAvailableBooks = (): Book[] => {
-  const { state } = useBookContext(); 
+  const { books } = useBookContext(); 
   const { loans } = useLoanManagement(); 
 
   const [availableBooks, setAvailableBooks] = useState<Book[]>([]);
 
   useEffect(() => {
     const borrowedBookIds = loans.map((loan) => Number(loan.bookId));
-    const books = state.books.filter(
+    const filteredBooks = books.filter(
       (book) =>
         book.quantity > 0 &&
         book.isAvailable &&
         !borrowedBookIds.includes(Number(book.id))
     );
-    setAvailableBooks(books);
-  }, [state.books, loans]);
+    setAvailableBooks(filteredBooks);
+  }, [books, loans]);
   
 
   return availableBooks;
